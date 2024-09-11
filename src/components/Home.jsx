33fe719472fcb49c73c4e54acc9e677a32fac809
Samir Warsi <warsisamir78.com>
2024-09-11 15:14:00 +0530

    new

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,32 @@
 import { useState } from "react";
 import Navbar from "./Navbar";
+
 export default function Home() {
   const [ipAddress, setIpAddress] = useState();
   const [ipData, setIpData] = useState({});
+  const [error, setError] = useState(false);
   const IpUrl = "http://ip-api.com/json";
 
-  async function GetData() {
-    const response = await fetch(`${IpUrl}/${ipAddress}`);
-    const jsonResponse = await response.json();
-    console.log(jsonResponse);
-    setIpData(jsonResponse);
-  }
+  // Function to validate IP address format
+  const isValidIP = (ipAddress) => {
+    const regex =
+      /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    return regex.test(ipAddress);
+  };
+
+  const GetData = async () => {
+    if (!isValidIP(ipAddress)) {
+      setError("Invalid IP address format. Please enter a valid IP address");
+      return;
+    }
+    try {
+      const response = await fetch(`${IpUrl}/${ipAddress}`);
+      const jsonResponse = await response.json();
+      setIpData(jsonResponse);
+    } catch (err) {
+      setError("no data found");
+    }
+  };
 
   let handleInput = (event) => {
     setIpAddress(event.target.value);
@@ -25,9 +41,14 @@ export default function Home() {
     <>
       <div className="main  h-[800px] sm:h-full  md:w-full bg-cover  bg-blue-600 bg-[url('/image/download.png')]">
         <Navbar />
+        {error && (
+          <div className="bg-red-100 text-red-700 p-2 sm:p-3 md:p-4 rounded w-[300px] sm:w-[400px] md:w-[500px] text-center mx-auto md:mb-4">
+            <p>{error}</p>
+          </div>
+        )}
         <div className="md:flex md:mt-5   mx-auto  md:gap-10 md:p-5 md:mx-10 ">
           <div className="leftside flex flex-col text-center w-1/2 mx-auto my-4 md:my-14 mt-0 sm:mt-10  justify-center text-4xl text-white md:mt-0 ">
-            <span>an API to find the </span>
+            <span>an APP to find the </span>
             <span>Ip Address</span>
           </div>
           <div className=" rightside  w-[350px] p-5 mx-auto rounded-lg   bg-red-300   md:p-10 text-center sm:w-[550px] md:w-[600px]">
@@ -41,16 +62,17 @@ export default function Home() {
                   onChange={handleInput}
                 />
                 <button
-                  className=" bg-purple-200 p-2 sm:p-4 md:p-4 rounded-lg text-black"
+                  className=" bg-purple-200 p-2 sm:p-4 md:p-4 rounded-lg text-blac k hover:bg-purple-300"
                   onClick={handleSubmit}
                 >
                   IPLookUp
                 </button>
+                
               </div>
               <div className="data">
                 <div className="p-4 sm:p-3 md:p-5 space-y-2 sm:space-y-3 md:space-y-5 ">
                   <div className="bg-white overflow-hidden text-black p-4 px-2 sm:px-4 rounded-md md:px-10 flex justify-between">
-                    <p className="text-start">ip address:</p>
+                    <p className="text-start">IP address:</p>
                     <p className="text-start">{ipData.query}</p>
                   </div>
                   <div className="bg-white overflow-hidden text-black p-4 px-2 sm:px-4 rounded-md md:px-10 flex justify-between">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,12 @@ export default function Navbar() {
       <div className="navbar flex justify-between md:w-full p-5 md:p-10 md:justify-evenly text-white">
         <div className=" flex  text-lg md:text-3xl ">
           <img src="/image/logo.png" className="h-10 md:h-14" />
-          <NavLink to="/"><h1>IP</h1></NavLink>
+          <NavLink to="/"><h1 >IP</h1></NavLink>
         </div>
         <div>
           <div className=" flex gap-2 md:gap-5  text-lg md:text-3xl ">
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="https://github.com/warsi0707/IpAddress">
-              GitHub
-            </NavLink>
+            <NavLink to="/about" className="hover:text-purple-500">About</NavLink>
+            <NavLink to="https://github.com/warsi0707/IpAddress" className="hover:text-gray-700 ">GitHub</NavLink>
           </div>
         </div>
       </div>
